addb3f2af634e251691641c422d86cc72eed81c2
ronakrifat5575 <rifathasanhome@gmail.com>
2025-02-13 13:32:10 +0600

    New professional website design

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    let heading = document.querySelector("h1");
-    heading.style.opacity = "0";
-    setTimeout(() => {
-        heading.style.transition = "opacity 2s";
-        heading.style.opacity = "1";
-    }, 500);
-});
